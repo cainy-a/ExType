b3097e138fba9d0e5b1d85b21793861c22897c81
cainy-a <yellowsink@protonmail.com>
2021-04-25 16:22:43 +0100

    XMLDOC

--- ExType.TypeConv/CustomTypeConv.cs
+++ ExType.TypeConv/CustomTypeConv.cs
@@ -4,10 +4,26 @@ using System.Reflection;
 
 namespace ExType.TypeConv
 {
+    /// <summary>
+    /// Convert between custom types !!! READ DOCS !!!
+    /// </summary>
     public static class CustomTypeConv
     {
+        /// <summary>
+        /// Converts to a specified target type that supports new() !!! READ DOCS !!!
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static T To<T>(this object obj) where T : new() => obj.To(new T());
 
+        /// <summary>
+        /// Converts to a specified target type using a given instance as a base !!! READ DOCS !!!
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="target"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static T To<T>(this object obj, T target)
         {
             var sourceMembers = obj.Members();
